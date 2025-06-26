4cd9036003a6634438b12cf5e22bb6e2daf43a44
jiyavyas <jiyavyas2811@gmail.com>
2025-06-26 11:33:22 +0530

    fixing bug

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const nodemailer = require("nodemailer");
+const path =require('path')
 
 // Load .env variables
 dotenv.config();
@@ -11,6 +12,9 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// static files
+app.use(express.static(path.join(__dirname, './client/build')))
 
 // POST route to handle email
 app.post("/send-email", async (req, res) => {
@@ -47,8 +51,12 @@ app.post("/send-email", async (req, res) => {
 // Portfolio route (if you have it)
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
 
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
+
 // Start server
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on PORT ${PORT}`);
 });
